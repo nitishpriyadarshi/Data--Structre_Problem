5578eb3b63a8c23d4660c110b79dbe4c23a67f2b
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-18 23:30:50 +0530

    UC1

---
+++ src/List/LinkedListMain.java
@@ -0,0 +1,14 @@
+package List;
+
+public class LinkedListMain {
+        public static void main(String[] args) {
+                LinkedList<Integer> list1 = new LinkedList();
+                list1.push(70);
+                list1.push(30);
+                list1.push(56);
+                list1.show();
+
+                System.out.println();
+        }
+}
+
