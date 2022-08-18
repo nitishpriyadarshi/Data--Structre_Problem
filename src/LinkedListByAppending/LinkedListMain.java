d1b742c9a9ed19d983adb3ee44613d9feabce2ae
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-18 23:48:46 +0530

    UC3

---
+++ src/LinkedListByAppending/LinkedListMain.java
@@ -0,0 +1,24 @@
+package LinkedListByAppending;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> list1 = new LinkedList();
+        list1.push(70);
+        list1.push(30);
+        list1.push(56);
+        list1.show();
+
+        System.out.println();
+        LinkedList<Integer> list2 = new LinkedList<>();
+        list2.add(56);
+        list2.add(30);
+        list2.add(70);
+        list2.show();
+
+        LinkedList<Integer>list3 = new LinkedList<>();
+        list3.append(56);
+        list3.append(30);
+        list3.append(70);
+        list3.show();
+    }
+}
---
+++ src/LinkedListByAppending/Node.java
@@ -0,0 +1,10 @@
+package LinkedListByAppending;
+
+public class Node <T> {
+    T data;
+    Node<T> next;
+
+    public Node(T data){
+        this.data = data;
+    }
+}
